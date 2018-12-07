7718277735ba9d8cd8a248ffc3bd5e9c14566843
tyrion.lv <tyrion.lv@perficient.com>
2018-12-07 17:24:06 +0800

    first commit

---
+++ src/main/java/top/grandma/lifefavorite/LifeFavoriteApplication.java
@@ -0,0 +1,12 @@
+package top.grandma.lifefavorite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeFavoriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeFavoriteApplication.class, args);
+	}
+}
---
+++ src/main/java/top/grandma/lifefavorite/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package top.grandma.lifefavorite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class HelloWorldController {
+
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "greeting";
+    }
+
+
+}
